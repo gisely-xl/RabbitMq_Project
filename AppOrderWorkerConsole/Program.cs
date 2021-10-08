99cbdd403bb9f9bd2c48c9d34f59087c5cffdee8
gisely-xl <83426796+gisely-xl@users.noreply.github.com>
2021-10-08 12:17:51 -0300

    First commit of an Console Consumer from RabbitMq

---
+++ AppOrderWorkerConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using AppOrderWorkerConsole.Domani;
+using AppOrderWorkerConsole.Repository;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace AppOrderWorkerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repository = new OrderRepository();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+
+                channel.QueueDeclare(queue: "orderQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        var order = JsonSerializer.Deserialize<Order>(message);
+
+                        repository.Create(order);
+                        //Console.WriteLine($"Order: {order.OrderNumber}| {order.ItemName}| {order.Price:N2} ");
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception e)
+                    {
+
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+
+                };
+                channel.BasicConsume(queue: "orderQueue",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ AppOrderWorkerConsole/Repository/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using AppOrderWorkerConsole.Domani;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppOrderWorkerConsole.Repository
+{
+    class OrderRepository
+    {
+        private readonly IMongoCollection<Order> _mongoCollection;
+        private static readonly string _mongoConnection = Environment.GetEnvironmentVariable("mongoConnection");
+        public OrderRepository()
+        {
+            var mongoClient = new MongoClient(_mongoConnection);
+            //var mongoClient = new MongoClient("mongodb+srv://xl:123xl@cluster0.08wko.mongodb.net/manageOrder?retryWrites=true&w=majority");
+            var dataBase = mongoClient.GetDatabase("manageOrder");
+            _mongoCollection = dataBase.GetCollection<Order>("Orders");
+        }
+        
+        public Order Create(Order order)
+        {
+            _mongoCollection.InsertOne(order);
+            return order;
+        }
+    }
+}
