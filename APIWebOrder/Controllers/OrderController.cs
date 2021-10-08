a2215d357eb58aeab47f0932a2e681e81b5b174a
gisely-xl <83426796+gisely-xl@users.noreply.github.com>
2021-10-08 12:15:10 -0300

    First commit of an simple API producer

---
+++ APIWebOrder/Controllers/OrderController.cs
@@ -0,0 +1,55 @@
+﻿using APIWebOrder.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace APIWebOrder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult IsertOrder(Order order)
+        {
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "orderQueue",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = JsonSerializer.Serialize(order);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "orderQueue",
+                                         basicProperties: null,
+                                         body: body);
+
+                }
+
+                return Accepted(order);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Erro ao tentar criar um novo pedido", e);
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
